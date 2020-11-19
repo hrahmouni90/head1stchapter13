44510c9ffc0aab9c0e1dbc3ebd846d15f95d9e4b
Hicha <hicrahtubi@gmail.com>
2020-11-19 10:32:16 +0100

    Initial commit

---
+++ app/src/main/java/com/hfad/head1stchapter13/Pizza.java
@@ -0,0 +1,29 @@
+package com.hfad.head1stchapter13;
+
+import com.hfad.head1stchapter13.R;
+
+/**
+ * Created by davidg on 04/05/2017.
+ */
+
+public class Pizza {
+    private String name;
+    private int imageResourceId;
+
+    public static final Pizza[] pizzas = {
+            new Pizza("Diavolo", R.drawable.diavolo),
+            new Pizza("Funghi", R.drawable.funghi)
+    };
+
+    private Pizza(String name, int imageResourceId) {
+        this.name = name;
+        this.imageResourceId = imageResourceId;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+}
